7b47ece8dee28800b98893b7d5c71a049a1a60df
chemicstry <chemicstry@gmail.com>
2022-08-08 15:52:18 +0300

    Fix example compilation

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,16 +1,18 @@
 #![no_std]
 #![no_main]
 
+use cortex_m_rt::entry;
+use defmt::*;
+use defmt_rtt as _;
+use hal::{i2c::I2c, pac, prelude::*};
 use panic_probe as _;
 use stm32f4xx_hal as hal;
-
-use cortex_m_rt::entry;
-
-use hal::{pac, prelude::*, i2c::I2c};
 
 #[entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
+
+    info!("Hello world!");
 
     // Set up the system clock. We want to run at 48MHz for this one.
     let rcc = dp.RCC.constrain();
@@ -30,7 +32,17 @@ fn main() -> ! {
         .internal_pull_up(true)
         .set_open_drain();
 
-    let _i2c = I2c::new(dp.I2C2, (scl, sda), 400.kHz(), &clocks);
+    let mut i2c = I2c::new(dp.I2C2, (scl, sda), 400.kHz(), &clocks);
 
-    unreachable!()
+    info!("Testing i2c");
+    for address in 0..127 {
+        let mut buf = [0u8; 1];
+        match i2c.read(address, &mut buf) {
+            Ok(_) => info!("i2c alive at: {}", address),
+            Err(_) => {}
+        }
+    }
+    info!("done");
+
+    defmt::unreachable!()
 }
